632e2e3a0bb37b8ff39a233ae95ebfd5fa2f36bd
MurrayChapman17 <murraychapman17@gmail.com>
2025-07-18 16:05:12 +0100

    trying removing boto3

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 # Test for deployment 2
-import boto3
+#import boto3
 
 from catalyst_ngd_wrappers.ngd_api_wrappers import items, items_limit, items_geom, \
     items_col, items_limit_geom, items_limit_col, items_geom_col, items_limit_geom_col
