542d97c1bc0a7fcc33a330d792d07531eb1f9844
MurrayChapman17 <murraychapman17@gmail.com>
2025-07-24 12:26:27 +0100

    test response

--- lambda_function.py
+++ lambda_function.py
@@ -187,6 +187,12 @@ def lambda_handler(event: dict, context) -> dict:
     AWS Lambda handler function.
     Routes the request to the appropriate function based on the event data.
     '''
+    return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(event)
+        }
 
     path = event['requestContext']['http']['path']
     parsed_path, collection = parse_base_path(path)
