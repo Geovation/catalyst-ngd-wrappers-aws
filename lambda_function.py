e3dff53a656355022c4b4e2e6195d9e00135ce2d
MurrayChapman17 <murraychapman17@gmail.com>
2025-08-07 11:59:59 +0100

    removing files now in other repository

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,12 @@
 import json
 
-from catalyst_ngd_wrappers.ngd_api_wrappers import items, items_limit, items_geom, \
+from catalyst_ngd_wrappers import items, items_limit, items_geom, \
     items_col, items_limit_geom, items_limit_col, items_geom_col, items_limit_geom_col
 
-from utils import BaseSerialisedRequest, handle_error, construct_features_response, \
+from catalyst_ngd_wrappers.utils import BaseSerialisedRequest, handle_error, construct_features_response, \
     construct_collections_response
 
-from schemas import FeaturesBaseSchema, LimitSchema, GeomSchema, ColSchema, \
+from catalyst_ngd_wrappers.schemas import FeaturesBaseSchema, LimitSchema, GeomSchema, ColSchema, \
     LimitGeomSchema, LimitColSchema, GeomColSchema, LimitGeomColSchema
 
 AWS_HEADERS = [
